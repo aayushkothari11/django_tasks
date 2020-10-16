be89612e06b4d4c7152f7ff873913917e6b96c83
aayush <aayushkothari11@yahoo.com>
2020-10-16 09:41:47 +0530

    Add comments in the script

--- Script/script.py
+++ Script/script.py
@@ -4,14 +4,35 @@ from datetime import datetime
 import pytz
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
+
+'''
+Global variable to store the details of the last time the file was updated.
+'''
 cached_stamp = 0
 
+
+'''
+An Asynchronous function to display the current time using the datetime module.
+
+Input - NA
+Output - prints the current time on the terminal
+'''
 async def time():
     IST = pytz.timezone('Asia/Kolkata')
     now = datetime.now(IST)
     current_time = now.strftime("%H:%M:%S")
     print("Current Time =", current_time)
 
+
+'''
+An Synchronous function that prints the content of the file on the terminal.
+The function only prints if the contents of the file have been changed.
+The value stored in cached_stamp represents the time when the file was last changed.
+The values of cached_stamp and os.stat(filename).st_mtime are compared to see if the file has been updated again or not.
+
+Input - NA
+Output - print the file content only if it is changed else no output
+'''
 async def file_data():
     global cached_stamp
     filename= './data.txt'
@@ -22,6 +43,11 @@ async def file_data():
             print(reader.read())
 
 
+'''
+Execution of the two above functions in an Asynchronous way.
+We create an object of AsyncIOScheduler class ie an async scheduler and add the two methods as jobs which will run in intervals of 10 seconds.
+We then create an asyncio reference which gets the event loop from the scheduler and runs the task forever until Cntl + C is pressed.
+'''
 if __name__ == '__main__':
     scheduler = AsyncIOScheduler()
     scheduler.add_job(time, 'interval', seconds=10)
